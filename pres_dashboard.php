3d7bcdecc0a2d7b9583a3f2d13b8971e98f422e8
SantiagoOcampo25 <101007618+SantiagoOcampo25@users.noreply.github.com>
2023-02-06 19:31:41 -0500

    Docs: Update Dasboard, Show Profile, Edit Profile

--- pres_dashboard.php
+++ pres_dashboard.php
@@ -71,59 +71,199 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-solid-rounded/css/uicons-solid-rounded.css'>
-
+    
     <link rel="stylesheet" href="./css/main.css">
     <link rel="stylesheet" href="./css/dashboardPaciente.css">
+    <link rel="stylesheet" href="./css/dashboardSecretaria.css">
+    <link rel="stylesheet" href="./css/dashboardDoctor.css">
+    <link rel="stylesheet" href="./css/dashboardAdministrador.css">
     <link rel="stylesheet" href="./css/components.css">
     <link rel="shortcut icon" href="./assets/img/icons/logo.svg" />
 
 </head>
 <body>
+    <div class="container-dashboard" style="transition: all 3s;">
 
-<div class="container-dashboard" style="transition: all 3s;">
+    <!-- Seccion Paciente -->
+
     <?php
-        include('./components/sidebar.php');
-        include('./components/navbar.php');
-    ?> 
-    <?php
-        if(isset($_SESSION['paciente'])){
-            if($_SESSION['paciente'] == 1){
-    ?>  
-    <div class="body-contenido">
-        <div class="con-dashboard-paciente">
-            <div class="con-citas">
-                <div class="con-form-citas">
-                    <div class="head-form">Citas médicas</div>
-                    <div class="body-form">Agenda tus citas médicas aquí.</div>
-                    <div class="foo-form">
-                        <button type="submit">Agendar cita</button>
+            if(isset($_SESSION['paciente'])){
+                if($_SESSION['paciente'] == 1){
+        ?>
+        <?php
+            include('./components/sidebar.php');
+            include('./components/navbar.php');
+         ?>   
+        <div class="body-contenido">
+            <div class="con-dashboard-paciente">
+                <div class="con-citas">
+                    <div class="con-form-citas">
+                        <div class="head-form">Citas médicas</div>
+                        <div class="body-form">Agenda tus citas médicas aquí.</div>
+                        <div class="foo-form">
+                            <button type="submit">Agendar cita</button>
+                        </div>
+                    </div>
+                    <div class="con-form-pqrsf">
+                        <div class="head-form">¿Problemas con Arca?</div>
+                        <div class="body-form">Ingresa al módulo de PQRSF.</div>
+                        <a href="" class="flecha-pqrsf">Módulo de PQRSF
+                            <img src="./assets/img/icons/flecha-pequena-derecha 1.svg" alt="De clic aquí para ingresar al módulo de PQRSF">
+                        </a>
+                        <a href="" class="emociones">
+                            <img src="./assets/img/emociones.png" alt="">
+                        </a>
                     </div>
                 </div>
-                <div class="con-form-pqrsf">
-                    <div class="head-form">¿Problemas con Arca?</div>
-                    <div class="body-form">Ingresa al módulo de PQRSF.</div>
-                    <a href="" class="flecha-pqrsf">Módulo de PQRSF
-                        <img src="./assets/img/icons/flecha-pequena-derecha 1.svg" alt="De clic aquí para ingresar al módulo de PQRSF">
-                    </a>
-                    <a href="" class="emociones">
-                        <img src="./assets/img/emociones.png" alt="">
-                    </a>
+                <div class="con-cm">
+                    <div class="title-cm">Citas medicas hoy</div>
+                    <?php include('pres_neg_dat_C_show.php');?>
                 </div>
-            </div>
-            <div class="con-cm">
-                <div class="title-cm">Mis citas médicas</div>
                 <?php
-                    include('pres_neg_dat_C_show.php');
+                        }
+                    }
+                ?>  
+
+            <!-- Seccion secretaria -->
+
+                <?php
+                    if(isset($_SESSION['secretaria'])){
+                        if($_SESSION['secretaria'] == 1){
                 ?>
+                <?php
+                    include('./components/sidebar.php');
+                    include('./components/navbar.php');
+                ?>   
+        <div class="body-contenido">
+            <div class="con-dashboard-secretaria">
+                <div class="con-citas-s">
+                    <div class="con-form-citas-s">
+                        <div class="head-form">Citas médicas</div>
+                        <div class="body-form">Agenda las citas médicas de pacientes aquí.</div>
+                        <div class="foo-form">
+                            <button type="submit">Agendar</button>
+                        </div>
+                    </div>
+                    <div class="con-form-users">
+                        <div class="head-form">Usuarios</div>
+                        <div class="body-form">Gestiona pacientes aqui.</div>
+                        <a href="" class="flecha-users">Gestionar
+                            <img src="./assets/img/icons/flecha-pequena-derecha 1.svg" alt="De clic aquí para ingresar al módulo de PQRSF">
+                        </a>
+                        <a href="" class="person">
+                            <img src="./assets/img/icons/empleados.svg" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="con-cm">
+                    <div class="title-cm">Citas medicas hoy</div>
+                    <?php include('pres_neg_dat_C_show.php');?>
+                </div>
+                <?php
+                        }
+                    }
+                ?> 
+
+            <!-- Seccion Doctor -->
+                
+                <?php
+                    if(isset($_SESSION['doctor'])){
+                        if($_SESSION['doctor'] == 1){
+                ?>
+                <?php
+                    include('./components/sidebar.php');
+                    include('./components/navbar.php');
+                ?>   
+        <div class="body-contenido">
+            <div class="con-dashboard-doctor">
+                <div class="con-horarios">
+                    <div class="con-form-horarios">
+                        <div class="head-form">Horarios</div>
+                        <div class="body-form">Agenda tus horarios medicos aqui.</div>
+                        <div class="foo-form">
+                            <button type="submit">Gestionar</button>
+                        </div>
+                    </div>
+                    <div class="con-form-diagnosticos">
+                        <div class="head-form">Diagnosticos</div>
+                        <div class="body-form">Gestiona tus diagnosticos medicos aqui.</div>
+                        <a href="" class="flecha-diagnosticos">Diagnosticos
+                            <img src="./assets/img/icons/flecha-pequena-derecha 1.svg" alt="De clic aquí para ingresar al módulo de PQRSF">
+                        </a>
+                        <a href="" class="info-diagnosticos">
+                            <img class="img-info-diagnosticos" src="./assets/img/icons/diagnostico 1.svg" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="con-cm">
+                    <div class="title-cm">Próximas citas médicas</div>
+                    <?php include('pres_neg_dat_C_show.php');?>
+                </div>
+                <?php
+                        }
+                    }
+                ?>     
+
+            <!-- Seccion Administrador -->
+
+                <?php
+                    if(isset($_SESSION['administrador'])){
+                        if($_SESSION['administrador'] == 1){
+                ?>
+                <?php
+                    include('./components/sidebar.php');
+                    include('./components/navbar.php');
+                ?>   
+        <div class="body-contenido">
+            <div class="con-dashboard-admin">
+                <div class="title-users">Usuarios
+                    <div class="view-users">
+                        <a href="pres_gestionUs.php" class="flecha-users">Gestionar usuarios
+                            <img src="./assets/img/icons/flecha-pequena-derecha 1.svg" alt="De clic aquí para ingresar al módulo de PQRSF">
+                        </a>
+                    </div>
+                </div>
+                <div class="con-info-users">
+                        <div class="num-pacientes">
+                            <a href="" class="icon">
+                                <img src="./assets/img/icons/Paciente.svg" alt="">
+                            </a>
+                            <div class="cont-user">34</div>
+                            <div class="name-user">Pacientes</div>
+                        </div>
+                        <div class="num-doctores">
+                            <a href="" class="icon">
+                                <img src="./assets/img/icons/Paciente+.svg" alt="">
+                            </a>
+                            <div class="cont-user">16</div>
+                            <div class="name-user">Doctores</div>
+                        </div>
+                        <div class="num-secretarias">
+                            <a href="" class="icon">
+                                <img src="./assets/img/icons/Secretaria-icon.svg" alt="">
+                            </a>
+                            <div class="cont-user">4</div>
+                            <div class="name-user">Secretarias</div>
+                        </div>
+                        <div class="num-administradores">
+                            <a href="" class="icon">
+                                <img src="./assets/img/icons/Paciente.svg" alt="">
+                            </a>
+                            <a href="" class="btn-admin">
+                                <img src="./assets/img/icons/ajustes 1.svg" alt="Administradores">
+                            </a>
+                            <div class="cont-user">3</div>
+                            <div class="name-user">Administradores</div>
+                        </div>
+                    </div>
+                </div>
+                <?php
+                        }
+                    }
+                ?> 
             </div>
-    <?php
-            }
-        }
-    ?>
         </div>
     </div>
-</div>
 
 <!-- JavaScript Bundle with Popper -->
 <script src="./js/sidebar.js"></script>
