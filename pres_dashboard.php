a89f6b688bae0f1de966b4348b2b4799fc282bb1
SantiagoOcampo25 <101007618+SantiagoOcampo25@users.noreply.github.com>
2023-02-06 20:21:44 -0500

    Docs: Update

--- pres_dashboard.php
+++ pres_dashboard.php
@@ -107,7 +107,7 @@
                     <div class="con-form-pqrsf">
                         <div class="head-form">¿Problemas con Arca?</div>
                         <div class="body-form">Ingresa al módulo de PQRSF.</div>
-                        <a href="" class="flecha-pqrsf">Módulo de PQRSF
+                        <a href="pres_pqrsf.html" class="flecha-pqrsf">Módulo de PQRSF
                             <img src="./assets/img/icons/flecha-pequena-derecha 1.svg" alt="De clic aquí para ingresar al módulo de PQRSF">
                         </a>
                         <a href="" class="emociones">
@@ -141,13 +141,15 @@
                         <div class="head-form">Citas médicas</div>
                         <div class="body-form">Agenda las citas médicas de pacientes aquí.</div>
                         <div class="foo-form">
-                            <button type="submit">Agendar</button>
+                            <a href="">
+                                <button type="submit">Agendar</button>
+                            </a>
                         </div>
                     </div>
                     <div class="con-form-users">
                         <div class="head-form">Usuarios</div>
                         <div class="body-form">Gestiona pacientes aqui.</div>
-                        <a href="" class="flecha-users">Gestionar
+                        <a href="pres_gestionUs.php" class="flecha-users">Gestionar
                             <img src="./assets/img/icons/flecha-pequena-derecha 1.svg" alt="De clic aquí para ingresar al módulo de PQRSF">
                         </a>
                         <a href="" class="person">
@@ -181,7 +183,7 @@
                         <div class="head-form">Horarios</div>
                         <div class="body-form">Agenda tus horarios medicos aqui.</div>
                         <div class="foo-form">
-                            <button type="submit">Gestionar</button>
+                            <button href="" type="submit">Gestionar</button>
                         </div>
                     </div>
                     <div class="con-form-diagnosticos">
@@ -270,3 +272,4 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
