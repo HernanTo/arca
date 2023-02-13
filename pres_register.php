bb8b369eb5dc157a35f1d4260efc8919acf8877d
SantiagoOcampo25 <101007618+SantiagoOcampo25@users.noreply.github.com>
2023-02-12 20:26:28 -0500

    Docs: Update

--- pres_register.php
+++ pres_register.php
@@ -1,10 +1,19 @@
-<?php
-        session_start();
-        if(isset($_SESSION['logueado'])){
-                if($_SESSION['logueado'] == 1){
-                        header('Location: pres_dashboard.php');
-                }
-        }
+<?php 
+include ('seguridad.php');
+if(isset($_SESSION['doctor'])){
+    if($_SESSION['doctor'] == 1){
+        header('Location: pres_dashboard.php');
+    }
+}
+if(isset($_SESSION['paciente'])){
+    if($_SESSION['paciente'] == 1){
+        header('Location: pres_dashboard.php');
+    }
+}
+
+$nameUs = $_SESSION["pNombre_U"];
+$titlePage = "Gestion de Horarios.";
+$descPage = "Gestiona los horarios de tus doctores y citas medicas.";
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -12,111 +21,121 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
+    <title>Usuarios</title>
     <link href="./bootstrap/bootstrap.min.css" rel="stylesheet">
-    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-solid-rounded/css/uicons-solid-rounded.css'>
 
     <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/gesHorarios.css">
+    <link rel="stylesheet" href="./css/components.css">
     <link rel="shortcut icon" href="./assets/img/icons/logo.svg" />
-    <link rel="stylesheet" href="./css/register.css">
-    <script src="./js/validateName.js"></script>
+
+    <!-- CSS only -->
+
 </head>
 <body>
-    <div class="con-register">
-        <div class="con-img-register">
-            <img src="./assets/img/Img-register.png" alt="">
-        </div>
-        <div class="head-register">
-            <div class="head-1">
-                <h2>Registrarse<span>.</span></h2>
+
+<div class="container-dashboard" style="transition: all 3s;">
+    <?php
+        include('./components/sidebar.php');
+        include('./components/navbar.php');
+        ?>
+        <div class="body-contenido">
+            <div class="con-cm-horarios">
+                <a href="" class="flecha-cm">Ver todas las citas medicas
+                    <img src="./assets/img/icons/flecha-pequena-derecha 1.svg" alt="De clic aquí para ver todas las citas medicas">
+                </a>
+                <div class="con-new-cm">
+                    <div class="con-cm">
+                        <div class="title-cm">Agregar cita medica.
+                            <a href="" class="agregar-cm">
+                                <img src="./assets/img/icons/agregar.svg" alt="">
+                            </a>
+                        </div>
+                        <div class="info-resp-cm">
+                            <input type="text" name="resp-cm" autocomplete="off" required>
+                            <label for="resp-cm" class="label-cm">
+                                <span class="span-cm">
+                                    Informacion del responsable de la cita.
+                                </span>
+                        </div>
+                        <div class="info-doc">
+                            <div class="especialidad-doc">
+                                <div class="form-floating">
+                                    <select class="form-select" id="floatingSelect" aria-label="Floating label select example" name="fk_pk_tipo_documentoU" required>
+                                        <option value="0">Ninguna</option>
+                                        <option value="1">Medico General</option>
+                                        <option value="2">Psicologo</option>
+                                    </select>
+                                    <label for="floatingSelect">especialidad</label>
+                                    <span class="ico-input ico-input-select"><img src="./assets/img/icons/desplegable.svg" alt=""></span>
+                                </div>          
+                            </div>
+                            <div class="name-doc">
+                                <div class="form-floating">
+                                    <select class="form-select" id="floatingSelect" aria-label="Floating label select example" name="" required>
+                                        <option value="0">1923938762 - Santiago Flores Gomez</option>
+                                        <option value="1">1923938762 - Santiago Flores Gomez</option>
+                                        <option value="2">1923938762 - Santiago Flores Gomez</option>
+                                    </select>
+                                    <label for="floatingSelect">Doctor</label>
+                                    <span class="ico-input ico-input-select"><img src="./assets/img/icons/desplegable.svg" alt=""></span>
+                                </div>       
+                            </div>
+                            <div class="consultorio-doc">
+                                <div class="form-floating">
+                                    <select class="form-select" id="floatingSelect" aria-label="Floating label select example" name="" required>
+                                        <option value="0">13</option>
+                                        <option value="1">18</option>
+                                        <option value="2">21</option>
+                                    </select>
+                                    <label for="floatingSelect">Consultorio</label>
+                                    <span class="ico-input ico-input-select"><img src="./assets/img/icons/desplegable.svg" alt=""></span>
+                                </div> 
+                            </div>
+                        </div>
+                        <div class="info-resp-cm">
+                            <input type="text" name="resp-cm" autocomplete="off" required>
+                            <label for="resp-cm" class="label-cm">
+                                <span class="span-cm">
+                                    Informacion del responsable de la cita.
+                                </span>
+                        </div>
+                        <div class="con-hora-agendamiento">
+                            <div class="semana-agendamiento">
+                                    <div class="form-floating">
+                                        <select class="form-select" id="floatingSelect" aria-label="Floating label select example" name="" required>
+                                            <option value="0">13 Febrero 2023 - 18 Febrero 2023</option>
+                                            <option value="1">13 Febrero 2023 - 18 Febrero 2023</option>
+                                            <option value="2">13 Febrero 2023 - 18 Febrero 2023</option>
+                                        </select>
+                                        <label for="floatingSelect">Consultorio</label>
+                                        <span class="ico-input ico-input-select"><img src="./assets/img/icons/desplegable.svg" alt=""></span>
+                                    </div>
+                            </div>
+                            <div class="horario-agendamiento">
+                                    <div class="form-floating">
+                                        <select class="form-select" id="floatingSelect" aria-label="Floating label select example" name="" required>
+                                            <option value="0">Mañana (8 am - 11 am)</option>
+                                            <option value="1">Mañana (8 am - 11 am)</option>
+                                            <option value="2">Mañana (8 am - 11 am)</option>
+                                        </select>
+                                        <label for="floatingSelect">Consultorio</label>
+                                        <span class="ico-input ico-input-select"><img src="./assets/img/icons/desplegable.svg" alt=""></span>
+                                    </div>
+                            </div>
+                        </div>
+                        <div class="con-btn-cm">
+                            <input id="btn-cm" type="submit" value="Registrar"></input>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="head-2">
-                <p>¿Ya tienes cuenta? <a href="./pres_login.php">Inicia sesión.</a></p>
-            </div>
         </div>
-        <div class="body-register">
-            <form action="./neg_dat_U_store.php" method="post" class="form-register">
-                <input type="hidden" name="type_register" value="1">
-                <div class="form-floating">
-                    <select class="form-select" id="floatingSelect" aria-label="Floating label select example" name="fk_pk_tipo_documentoU" required>
-                      <option value="CC">Cédula de ciudadanía</option>
-                      <option value="TI">Tarjeta de identidad</option>
-                      <option value="CE">Cédula de extranjería</option>
-                    </select>
-                    <label for="floatingSelect">Tipo de documento</label>
-                    <span class="ico-input ico-input-select"><img src="./assets/img/icons/desplegable.svg" alt=""></span>
-                </div>
+</div>
 
-                  <div class="form-floating">
-                    <input type="number" class="form-control number-input" id="floatingInputValue" placeholder="Número de identidad" name="documento" maxlength="10" required>
-                    <label for="floatingInputValue">Número de identidad</label>
-                    <span class="ico-input"><img src="./assets/img/icons/id-insignia 1.svg" alt=""></span>
-                </div>
 
-                <div class="form-floating">
-                    <input type="text" class="form-control" placeholder="Primer nombre" id="pr_name" name="pr_name" onblur="validateName('pr_name', 'Por favor, introduzca solo letras en el campo de primer nombre')" maxlength="15" required>
-                    <label for="pr_name">Primer nombre</label>
-                    <span class="ico-input"><img src="./assets/img/icons/retratoIn.svg" alt=""></span>
-                </div>
-
-                <div class="form-floating">
-                    <input type="text" class="form-control" placeholder="Segundo nombre" id="sg_name" name="sg_name" onblur="validateName('sg_name', 'Por favor, introduzca solo letras en el campo de segundo nombre')" maxlength="15">
-                    <label for="sg_name">Segundo nombre</label>
-                    <span class="ico-input"><img src="./assets/img/icons/retratoIn.svg" alt=""></span>
-                </div>
-
-                <div class="form-floating">
-                    <input type="text" class="form-control" placeholder="Primer apellido" id="pr_apellido" name="pr_apellido" onblur="validateName('pr_apellido', 'Por favor, introduzca solo letras en el campo de primer apellido')" maxlength="15" required>
-                    <label for="pr_apellido">Primer apellido</label>
-                    <span class="ico-input"><img src="./assets/img/icons/retratoIn.svg" alt=""></span>
-                </div>
-
-                <div class="form-floating">
-                    <input type="text" class="form-control" placeholder="Segundo apellido" id="sg_apellido" name="sg_apellido" onblur="validateName('sg_apellido', 'Por favor, introduzca solo letras en el campo de segundo apellido')" maxlength="15">
-                    <label for="sg_apellido">Segundo apellido</label>
-                    <span class="ico-input"><img src="./assets/img/icons/retratoIn.svg" alt=""></span>
-                </div>
-
-                <div class="form-floating">
-                    <input type="date" class="form-control date-input" id="floatingInputValue" placeholder="Fecha de nacimiento" name="fecha_naci" required>
-                    <label for="floatingInputValue">Fecha de nacimiento</label>
-                    <span class="ico-input"><img src="./assets/img/icons/date.svg" alt=""></span>
-                </div>
-
-                <div class="form-floating">
-                    <input type="number" class="form-control number-input" id="floatingInputValue" placeholder="Teléfono" name="Numcel" maxlength="10" required>
-                    <label for="floatingInputValue">Telefono</label>
-                    <span class="ico-input"><img src="./assets/img/icons/telefono.svg" alt=""></span>
-                </div>
-
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="floatingInputValue" placeholder="Direccion de residencia" name="direccionU" required>
-                    <label for="floatingInputValue">Dirección de residencia</label>
-                    <span class="ico-input"><img src="./assets/img/icons/location.svg" alt=""></span>
-                </div>
-
-                <div class="form-floating">
-                    <input type="email" class="form-control" id="floatingInputValue" placeholder="Correo electronico" name="correo" required>
-                    <label for="floatingInputValue">Correo electrónico</label>
-                    <span class="ico-input"><img src="./assets/img/icons/sobre 1.svg" alt=""></span>
-                </div>
-
-                <div class="form-floating">
-                    <input type="password" class="form-control" id="floatingInputValue" placeholder="Contraseña" name="claveU" maxlength="15" required>
-                    <label for="floatingInputValue">Contraseña</label>
-                </div>
-
-                <div class="form-floating">
-                    <input type="password" class="form-control" id="floatingInputValue" placeholder="Confirmar Contraseña" maxlength="15">
-                    <label for="floatingInputValue">Confirmar Contraseña</label>
-                </div>
-
-                <div class="con-btn-register">
-                    <input id="btn-register" type="submit" value="Registrarse"></input>
-                </div>
-            </form>
-        </div>
-    </div>
+<!-- JavaScript Bundle with Popper -->
+<script src="./js/sidebar.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
